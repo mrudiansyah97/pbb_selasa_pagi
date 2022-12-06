bd067cc7201dc368a6c87908e34a88134b7959ed
unknown <rudiansyah5553@gmail.com>
2022-12-06 11:26:49 +0700

    Pertemuan 10

---
+++ latihan_project/lib/pages/foodlistview.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import '/pages/itemcard.dart';
+
+
+class FoodListView extends StatelessWidget {
+  const FoodListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Container(
+        height: 160.0,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            ItemCard(),
+            ItemCard(),
+            ItemCard(),
+            ItemCard(),
+          ],
+        )
+      ),
+    );
+  }
+}
